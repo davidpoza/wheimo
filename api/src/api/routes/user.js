e7bd7ff58e29e8bd2657a2a796bfc55c6a546a41
David Poza Suárez <pozasuarez@gmail.com>
2021-01-12 15:01:07 +0100

    user list all and list by id endpoint

--- api/src/api/routes/user.js
+++ api/src/api/routes/user.js
@@ -7,15 +7,31 @@ const route = Router();
 
 export default (app) => {
   app.use('/user', route);
-  route.get('/', middlewares.isAuth, (req, res, next) => {
-    res.send('protected endpoint');
-  });
+
   route.post('/', async (req, res, next) => {
     const userService = Container.get('userService');
     await userService.create(
       { email: 'pozasuarez@gmail.com', name: 'prueba', password: '1234', active: true, level: 'admin' }
     );
     res.send('usuario creado');
+  });
+
+  route.get('/:id?', middlewares.isAuth, async (req, res, next) => {
+    const { id } = req.params
+    const userService = Container.get('userService');
+    try {
+      if (id) {
+        const user = await userService.findById(id);
+        if (!user) {
+          return res.sendStatus(404);
+        }
+        return res.status(200).json(user);
+      }
+      const users = await userService.findAll();
+      return res.status(200).json(users);
+    } catch (err) {
+      return next(err);
+    }
   });
 
 };
--- api/src/services/user.js
+++ api/src/services/user.js
@@ -1,8 +1,11 @@
+import e from 'express';
 import { Container } from 'typedi';
 
 export default class UserService {
   constructor() {
     this.sequelize = Container.get('sequelizeInstance');
+    this.logger = Container.get('loggerInstance');
+    this.userModel = this.sequelize.models.users;
   }
   async create({
     email,
@@ -14,7 +17,49 @@ export default class UserService {
     try {
       await this.sequelize.models.users.create({ email, name, password, active, level });
     } catch (err) {
-      console.log(err)
+      this.logger.error(e);
+      throw e;
+    }
+  }
+
+  async findAll() {
+    try {
+      const users = await this.sequelize.models.users.findAll({  });
+      return users.map((u) => {
+        return ({
+          id: u.id,
+          name: u.name,
+          level: u.level,
+          email: u.email,
+          active: u.active,
+          created_at: u.created_at,
+          updated_at: u.updated_at,
+        });
+      });
+    } catch (err) {
+      this.logger.error(e);
+      throw e;
+    }
+  }
+
+  async findById(id) {
+    try {
+      const user = await this.sequelize.models.users.findOne({ where: { id } });
+      if (!user) {
+        return null;
+      }
+      return ({
+        id: user.dataValues.id,
+        name: user.dataValues.name,
+        level: user.dataValues.level,
+        email: user.dataValues.email,
+        active: user.dataValues.active,
+        created_at: user.dataValues.created_at,
+        updated_at: user.dataValues.updated_at,
+      });
+    } catch (err) {
+      this.logger.error(e);
+      throw e;
     }
   }
 };
