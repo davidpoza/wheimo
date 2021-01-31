0ac77a5a0c5082b8671a1376f65d270e8b14f292
David Poza Suárez <pozasuarez@gmail.com>
2021-01-31 13:41:52 +0100

    added sample tags and rules to db-init

--- api/scripts/db-init.js
+++ api/scripts/db-init.js
@@ -10,6 +10,7 @@ import { definition as recurrentPaymentDefinition } from '../src/models/recurren
 import { definition as ruleDefinition } from '../src/models/rule.js';
 import { definition as taggedDefinition } from '../src/models/tagged.js';
 import { definition as appliedRulesDefinition } from '../src/models/applied-rules.js';
+import { definition as budgetDefinition } from '../src/models/budget.js';
 
 const sequelize = sequelizeLoader.newConnection();
 const queryInterface = sequelize.getQueryInterface();
@@ -22,6 +23,7 @@ queryInterface.dropTable('recurrents');
 queryInterface.dropTable('rules');
 queryInterface.dropTable('tagged');
 queryInterface.dropTable('appliedRules');
+queryInterface.dropTable('budgetDefinition');
 
 queryInterface.createTable(...userDefinition);
 queryInterface.createTable(...accountDefinition);
@@ -31,6 +33,7 @@ queryInterface.createTable(...recurrentPaymentDefinition);
 queryInterface.createTable(...ruleDefinition);
 queryInterface.createTable(...taggedDefinition);
 queryInterface.createTable(...appliedRulesDefinition);
+queryInterface.createTable(...budgetDefinition);
 
 sequelize.models.users.create({
   email: 'admin@gmail.com', name: 'admin', password: bcrypt.hashSync('admin', config.bcryptRounds, config.bcryptSalt), active: true, level: 'admin'
@@ -42,4 +45,24 @@ sequelize.models.accounts.create({
 
 sequelize.models.accounts.create({
   name: 'openbank compras', description: 'compras en internet', number: 'ES27 0073 0100 5504 7468 0011', bankId: 'opbk',  userId: 1
-})
+});
+
+sequelize.models.tags.create({
+  name: 'pagos de pepe de más de 5 euros', userId: 1
+});
+
+sequelize.models.rules.create({
+  name: 'pagos de pepe', type: 'emitterName', value: 'Pepe', userId: 1
+});
+
+sequelize.models.rules.create({
+  name: 'transacciones de más de 5 euros', type: 'amount', value: 'gt5', userId: 1
+});
+
+sequelize.models.appliedRules.create({
+  ruleId: 1, tagId: 1
+});
+
+sequelize.models.appliedRules.create({
+  ruleId: 2, tagId: 1
+});
