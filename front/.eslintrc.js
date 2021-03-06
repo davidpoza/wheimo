8dd7a2bc7e21d70141f5accc18263062f109a372
David Poza Suárez <pozasuarez@gmail.com>
2021-03-06 22:04:11 +0100

    linter config

---
+++ front/.eslintrc.js
@@ -0,0 +1,47 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+    commonjs: true,
+    es2021: true,
+  },
+  settings: {
+    'import/resolver': {
+      webpack: {
+        config: {
+          resolve: {
+            extensions: [
+              '.js',
+              '.jsx',
+            ],
+          },
+        },
+      },
+    },
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'max-len': [2, 120, 2],
+    'import/extensions': ['error', 'always', {
+      ts: 'never',
+      tsx: 'never',
+      js: 'never',
+      jsx: 'never',
+    }],
+    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+    'import/no-unresolved': ['error'],
+  },
+};
