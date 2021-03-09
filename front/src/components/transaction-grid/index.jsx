fe9d2ec9d85102141a68e71ba2423af892707dab
David Poza Suárez <pozasuarez@gmail.com>
2021-03-09 21:52:26 +0100

    transaction grid item count calculated

--- front/src/components/transaction-grid/index.jsx
+++ front/src/components/transaction-grid/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Proptypes from 'prop-types';
 import List from '@material-ui/core/List';
 import Pagination from '@material-ui/lab/Pagination';
@@ -6,17 +6,23 @@ import Pagination from '@material-ui/lab/Pagination';
 // own
 import TransactionGridItem from '../transaction-grid-item';
 import useStyles from './styles';
+import useWindowSize from '../../hooks/use-window-size';
 
 function TransactionGrid({ transactions }) {
   const classes = useStyles();
-  const PAGE_SIZE = 8; // TODO: recalculate this number with viewport height
+  const windowSize = useWindowSize();
+  const HEADER_FOOTER_SIZE = 310; // TODO: this value should be calculated
+  const ITEM_SIZE = 81; // TODO: this value should be calculated
+  const PAGE_SIZE = windowSize
+    ? Math.round((windowSize.height - HEADER_FOOTER_SIZE) / ITEM_SIZE)
+    : 0;
   const [page, setPage] = useState(1);
 
   function handlePageChange(event, value) {
     setPage(value);
   }
 
-  const chunk = transactions
+  const chunk = (transactions && PAGE_SIZE)
     ? transactions.slice((page - 1) * PAGE_SIZE, (page - 1) * PAGE_SIZE + PAGE_SIZE)
     : [];
 
