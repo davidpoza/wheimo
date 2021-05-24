915ad0a176b4339b8e95994f2908841b25d1f429
David Poza Suárez <pozasuarez@gmail.com>
2021-05-24 19:08:48 +0200

    cors config

--- api/src/loaders/express.js
+++ api/src/loaders/express.js
@@ -9,7 +9,10 @@ import config from '../config/config.js';
 export default ({ app }) => {
   // It shows the real origin IP if behind proxy
   app.enable('trust proxy');
-  app.use(cors());
+  app.use(cors({
+    origin: [config.url, `http://localhost:${config.port}`],
+    optionsSuccessStatus: 200,
+  }));
   app.use(bodyParser.json());
   app.use(config.api.prefix, routes());
   app.use(errors());
