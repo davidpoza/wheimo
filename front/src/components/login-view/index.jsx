f65fb52212c166e9b53d3f4326c405b2b6aaf3f6
David Poza Suárez <pozasuarez@gmail.com>
2021-11-28 20:12:39 +0100

    login error msg and fix for index passed to edit-tag-dialog

--- front/src/components/login-view/index.jsx
+++ front/src/components/login-view/index.jsx
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 import get from 'lodash.get';
 
 // material ui
+import Snackbar from '@material-ui/core/Snackbar';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 import TextField from '@material-ui/core/TextField';
@@ -17,6 +18,7 @@ function Login({ getAuth, user, history }) {
   const classes = useStyles();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [snackbarOpen, setSnackbarOpen] = useState(false);
 
   useEffect(() => {
     if (get(user, 'token')) {
@@ -24,31 +26,55 @@ function Login({ getAuth, user, history }) {
     }
   }, [user]);
 
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setSnackbarOpen(false);
+  };
+
   async function onFormSubmit(e) {
     e.preventDefault();
-    getAuth(email, password);
+    getAuth(email, password)
+      .catch(err => {
+        setSnackbarOpen(true);
+      })
   }
 
   return (
-    <Grid container direction="column" justify="center" alignItems="center" style={{ minHeight: '100vh' }}>
-      <Grid item xs={12} lg={5} className={classes.root}>
-        <form noValidate onSubmit={onFormSubmit}>
-          <Typography variant="h1" className={classes.title}>Where Is My Money</Typography>
-          <div>
-            <TextField
-              id="email" label="Email" variant="outlined" className={classes.email} autoFocus={true}
-              value={email} onChange={(e) => setEmail(e.target.value)} />
-          </div>
-          <div>
-            <TextField id="password" type="password" label="Password" variant="outlined" className={classes.password}
-              value={password} onChange={(e) => setPassword(e.target.value)} />
-          </div>
-          <Button variant="contained" color="primary" type="submit" disableElevation disabled={!email || !password }>
-            Login
-          </Button>
-        </form>
+    <>
+      <Grid container direction="column" justify="center" alignItems="center" style={{
+        position: 'absolute',
+        left: '50%',
+        top: '50%',
+        transform: 'translate(-50%, -50%)',
+      }}>
+        <Grid item xs={12} lg={5} className={classes.root}>
+          <form noValidate onSubmit={onFormSubmit}>
+            <Typography variant="h1" className={classes.title}>Where Is My Money</Typography>
+            <div>
+              <TextField
+                id="email" label="Email" variant="outlined" className={classes.email} autoFocus={true}
+                value={email} onChange={(e) => setEmail(e.target.value)} />
+            </div>
+            <div>
+              <TextField id="password" type="password" label="Password" variant="outlined" className={classes.password}
+                value={password} onChange={(e) => setPassword(e.target.value)} />
+            </div>
+            <Button variant="contained" color="primary" className={classes.button} type="submit" disableElevation disabled={!email || !password }>
+              Login
+            </Button>
+          </form>
+        </Grid>
       </Grid>
-    </Grid>
+      <Snackbar
+        open={snackbarOpen}
+        autoHideDuration={2000}
+        onClose={handleClose}
+        message="Wrong email or password"
+    />
+    </>
   );
 }
 
@@ -67,10 +93,7 @@ const mapStateToProps = (state) => ({
 
 const mapDispatchToProps = (dispatch) => ({
   getAuth: (email, password) => {
-    dispatch(getAuthAction(email, password))
-      .catch((error) => {
-        console.log(error.message);
-      });
+    return dispatch(getAuthAction(email, password))
   },
 });
 
