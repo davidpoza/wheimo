43fffd983dfbe5775a6531cc954fb30c71af457c
David Poza Suárez <pozasuarez@gmail.com>
2021-12-25 12:27:01 +0100

    good solution for heatmap navigation

--- front/src/components/heatmap-view/index.jsx
+++ front/src/components/heatmap-view/index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useMemo } from 'react';
 import PropTypes from 'prop-types';
 import dayjs from 'dayjs';
 import { connect } from 'react-redux';
@@ -13,30 +13,65 @@ import withLoader from '../../hocs/with-loader';
 import useStyles from './styles';
 import { fetchAll } from 'api-client/heatmap';
 
+
 function HeatmapView({
   user, isMobile
 }) {
   const classes = useStyles();
-  const [timeWindow, setTimeWindow] = useState(isMobile ? 6 : 12); // in months, 6 in mobile
-  const [from, setFrom] = useState(dayjs().subtract(timeWindow, 'months').format('YYYY-MM-DD'));
-  const [to, setTo] = useState(dayjs().format('YYYY-MM-DD'));
+  const [index, setIndex] = useState();
   const [rawData, setRawData] = useState([]);
+
+
+  const calculateDateRangeList = (isMob) => {
+    const years = [...Array(10).keys()].map(y => y + 2020);
+    const ret = [];
+    years.forEach((y) => {
+      if (isMob) {
+        ret.push({
+          year: y,
+          from: `${y}-01-01`,
+          to: `${y}-06-30`,
+        });
+        ret.push({
+          year: y,
+          from: `${y}-07-01`,
+          to: `${y}-12-31`,
+        });
+      } else {
+        ret.push({
+          year: y,
+          from: `${y}-01-01`,
+          to: `${y}-12-31`,
+        });
+      }
+    })
+    return (ret);
+  };
+
+  const currentYear = parseInt(dayjs().format('YYYY'), 10);
+  const rangesList = useMemo(() => calculateDateRangeList(isMobile), [isMobile]);
+  const { from, to } = rangesList?.[index] || {};
+
+  useEffect(() => {
+    setIndex(rangesList.findIndex(r => r.year === currentYear && dayjs(r.to).diff(dayjs()) >= 0)); // we choose the range with is not passed yet
+  }, [currentYear, rangesList]);
 
   useEffect(() => {
     (async () => {
       if (user?.token) setRawData(await fetchAll(user?.token, { from, to }));
     })();
-  }, [setRawData, user, from, to]);
+  }, [setRawData, user, index, from, to]);
+
+
 
   const moveBack = () => {
-    setFrom(dayjs(from, 'YYYY-MM-DD').subtract(timeWindow, 'months').format('YYYY-MM-DD'));
-    setTo(dayjs(to, 'YYYY-MM-DD').subtract(timeWindow, 'months').format('YYYY-MM-DD'));
+    if (index > 0) setIndex(index - 1);
   };
 
   const moveForward = () => {
-    setFrom(dayjs(from, 'YYYY-MM-DD').add(timeWindow, 'months').format('YYYY-MM-DD'));
-    setTo(dayjs(to, 'YYYY-MM-DD').add(timeWindow, 'months').format('YYYY-MM-DD'));
+    if (index < rangesList.length-1) setIndex(index + 1);
   };
+
 
   return (
     <div className={classes.root}>
@@ -45,7 +80,7 @@ function HeatmapView({
         <IconButton aria-label="back" className={classes.margin} size="small" onClick={moveBack}>
           <ArrowBackIcon fontSize="inherit" />
         </IconButton>
-        <span>{dayjs(from, 'YYYY-MM-DD').format('YYYY')}</span>
+        <span>{from ? dayjs(from, 'YYYY-MM-DD').format('YYYY') : 'loading...'}</span>
         <IconButton aria-label="forward" className={classes.margin} size="small" onClick={moveForward}>
           <ArrowForwardIcon fontSize="inherit" />
         </IconButton>
