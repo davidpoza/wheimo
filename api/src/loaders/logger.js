c95f5e2cd9d63c468d7a0e1e1e701b64d0549092
David Poza Suárez <pozasuarez@gmail.com>
2022-05-16 17:41:50 +0200

    ip to the real log file not the console

--- api/src/loaders/logger.js
+++ api/src/loaders/logger.js
@@ -2,22 +2,16 @@ import winston from 'winston';
 import config from '../config/config.js';
 import 'winston-daily-rotate-file';
 
-const transports = [];
-if(process.env.NODE_ENV === 'development') {
-  transports.push(
-    new winston.transports.Console()
-  )
-} else {
-  transports.push(
-    new (winston.transports.DailyRotateFile)({
-      filename: 'logs/%DATE%.log',
-      datePattern: 'YYYY-MM-DD',
-      zippedArchive: true,
-      maxSize: '20m',
-      maxFiles: '14d'
-    })
-  )
-}
+const transports = [
+  new winston.transports.Console(),
+  new (winston.transports.DailyRotateFile)({
+    filename: 'logs/%DATE%.log',
+    datePattern: 'YYYY-MM-DD',
+    zippedArchive: true,
+    maxSize: '20m',
+    maxFiles: '14d'
+  })
+];
 
 function levelIcon(level) {
   switch (level) {
